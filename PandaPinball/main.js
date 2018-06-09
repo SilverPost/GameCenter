b1553d101db889bd78561972a7467e4cd94f8d43
SilverPost <object.trade.sort+100@gmail.com>
2018-06-09 19:14:29 +0900

    [PandaPinball] The wall that constitutes a stage.

--- PandaPinball/main.js
+++ PandaPinball/main.js
@@ -11,9 +11,29 @@ var BALL_SIZE     = 28;
 var PIN_SIZE      = 26;
 
 /*
- * pins
+ * wall
  */
-phina.define("Pin", {
+phina.define("Box2dWall", {
+  superClass: "RectangleShape",
+  
+  init: function(layer, x, y, width, height) {
+    this.superInit();
+    this.fill = 'brown';
+    this.setPosition(x, y);
+    this.width = width;
+    this.height = height;
+    // create Box2d object
+    layer.createBody({
+      type: 'static', 
+      shape: 'box',
+    }).attachTo(this);
+  },
+});
+
+/*
+ * pin
+ */
+phina.define("Box2dPin", {
   superClass: "CircleShape",
   
   init: function(layer, color, x, y, size) {
@@ -21,8 +41,6 @@ phina.define("Pin", {
     this.fill = color;
     this.setPosition(x, y);
     this.radius = size;
-    // to show Box2d debug menu
-    this.alpha = 0.5;
     // create Box2d object
     layer.createBody({
       type: 'static', 
@@ -44,7 +62,7 @@ phina.define("MainScene", {
     this.backgroundColor = 'black';
     
     // create Box2d layer
-    var layer = Box2dLayer({
+    this.layer = Box2dLayer({
       width: SCREEN_WIDTH,
       height: SCREEN_HEIGHT,
     }).addChildTo(this);
@@ -52,23 +70,33 @@ phina.define("MainScene", {
     // ball
     var ball = CircleShape().addChildTo(this);
     ball.radius = BALL_SIZE;
-    ball.setPosition(SCREEN_WIDTH*0.3, PIN_SIZE/2);
-    // to show Box2d debug menu
-    ball.alpha = 0.5;
+    ball.setPosition(SCREEN_WIDTH*0.3, SCREEN_HEIGHT*0.2);
     // create Box2d object
-    layer.createBody({
+    this.layer.createBody({
       type: 'dynamic', 
       shape: 'circle',
     }).attachTo(ball);
     
-    // Pins
+    // create stage
+    this.stage();
+    
+    // pins
     var pin_x = [SCREEN_WIDTH*0.25, SCREEN_WIDTH*0.4, SCREEN_WIDTH*0.4, 
                  SCREEN_WIDTH*0.6, SCREEN_WIDTH*0.75, SCREEN_WIDTH*0.7];
     var pin_y = [SCREEN_HEIGHT*0.4, SCREEN_HEIGHT*0.5, SCREEN_HEIGHT*0.7,
                  SCREEN_HEIGHT*0.55, SCREEN_HEIGHT*0.45, SCREEN_HEIGHT*0.75];
     for (var i = 0; i < pin_x.length; i++) {
-      Pin(layer, 'yellow', pin_x[i], pin_y[i], PIN_SIZE).addChildTo(this);
+      Box2dPin(this.layer, 'yellow', pin_x[i], pin_y[i], PIN_SIZE).addChildTo(this);
     }
+  },
+  
+  stage: function() {
+    Box2dWall(this.layer, SCREEN_WIDTH*0.5, SCREEN_HEIGHT*0.025,
+                          SCREEN_WIDTH, SCREEN_HEIGHT*0.05).addChildTo(this);
+    Box2dWall(this.layer, SCREEN_WIDTH*0.025, SCREEN_HEIGHT*0.525,
+                          SCREEN_WIDTH*0.05, SCREEN_HEIGHT*0.95).addChildTo(this);
+    Box2dWall(this.layer, SCREEN_WIDTH*0.975, SCREEN_HEIGHT*0.525,
+                          SCREEN_WIDTH*0.05, SCREEN_HEIGHT*0.95).addChildTo(this);
   },
 });
 
@@ -79,7 +107,7 @@ phina.main(function() {
   // create application
   var app = GameApp({
     title: 'Panda Pinball',
-    startLabel: 'title',
+    startLabel: 'main',
     width: SCREEN_WIDTH,
     height: SCREEN_HEIGHT,
   });
