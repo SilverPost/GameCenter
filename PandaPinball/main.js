63fe9af0389d313de80414e9fceaa9a62df5d312
SilverPost <object.trade.sort+100@gmail.com>
2018-06-09 16:29:15 +0900

    [PandaPinball] The ball falls free and the pin plays it.

--- PandaPinball/main.js
+++ PandaPinball/main.js
@@ -5,8 +5,10 @@
 phina.globalize();
 
 // size information
-var SCREEN_WIDTH    = 600;
-var SCREEN_HEIGHT   = 960;
+var SCREEN_WIDTH  = 600;
+var SCREEN_HEIGHT = 960;
+var BALL_SIZE     = 32;
+var PIN_SIZE      = 26;
 
 /*
  * main scene
@@ -16,6 +18,40 @@ phina.define("MainScene", {
   
   init: function(options) {
     this.superInit(options);
+    
+    // background
+    this.backgroundColor = 'black';
+    
+    // create Box2d layer
+    var layer = Box2dLayer({
+      width: SCREEN_WIDTH,
+      height: SCREEN_HEIGHT,
+    }).addChildTo(this);
+    
+    // ball
+    var ball = CircleShape().addChildTo(this);
+    ball.radius = BALL_SIZE;
+    ball.setPosition(SCREEN_WIDTH*0.3, SCREEN_HEIGHT*0.3);
+    // to show Box2d debug menu
+    ball.alpha = 0.5;
+    // create Box2d object
+    layer.createBody({
+      type: 'dynamic', 
+      shape: 'circle',
+    }).attachTo(ball);
+    
+    // pin
+    var pin = CircleShape().addChildTo(this);
+    pin.fill = 'yellow'
+    pin.radius = PIN_SIZE;
+    pin.setPosition(SCREEN_WIDTH*0.28, SCREEN_HEIGHT*0.5);
+    // to show Box2d debug menu
+    pin.alpha = 0.5;
+    // create Box2d object
+    layer.createBody({
+      type: 'static', 
+      shape: 'circle',
+    }).attachTo(pin);
   },
 });
 
