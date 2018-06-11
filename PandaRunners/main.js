ee7e17153de52f1a0f9778b49431e14d0dcf3c5a
SilverPost <object.trade.sort+100@gmail.com>
2018-06-12 02:57:13 +0900

    [PandaRunners] Add an image file and a text file.

--- PandaRunners/main.js
+++ PandaRunners/main.js
@@ -8,6 +8,27 @@ phina.globalize();
 var SCREEN_WIDTH      = 1280; // 32*40
 var SCREEN_HEIGHT     = 640;  // 32*20
 
+var ASSETS = {
+  image: {
+    mountain_img: "https://raw.githubusercontent.com/SilverPost/GameCenter/master/PandaRunners/image/sprite_mountain.png",
+  },
+  text: {
+    mountain_txt: "https://raw.githubusercontent.com/SilverPost/GameCenter/master/PandaRunners/stage/mountain.txt",
+  },
+};
+
+/*
+ * create stage
+ */
+phina.define("Stage", {
+  superClass: 'RectangleShape',
+  
+  init: function() {
+    this.stageText = AssetManager.get('text', 'mountain_txt').data;
+    console.log(this.stageText);
+  },
+})
+
 /*
  * main scene
  */
@@ -17,8 +38,7 @@ phina.define("MainScene", {
   init: function(options) {
     this.superInit(options);
     
-    // background
-    this.backgroundColor = 'black';
+    Stage();
   },
   
   update: function(app) {
@@ -33,9 +53,10 @@ phina.main(function() {
   var app = GameApp({
     title: 'Panda Runners',
     startLabel: 'title',
-    backgroundColor: 'black',
+    backgroundColor: 'skyblue',
     width: SCREEN_WIDTH,
     height: SCREEN_HEIGHT,
+    assets: ASSETS,
   });
   
   // enable FPS
