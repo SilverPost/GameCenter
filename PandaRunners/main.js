17bda8961a792e6cb8fecf80de37f7e56f705e70
SilverPost <object.trade.sort+100@gmail.com>
2018-06-14 23:50:36 +0900

    [PandaRunners] The player stands on a stage.

--- PandaRunners/main.js
+++ PandaRunners/main.js
@@ -7,16 +7,46 @@ phina.globalize();
 // size information
 var SCREEN_WIDTH  = 1280; // 32*40
 var SCREEN_HEIGHT = 640;  // 32*20
-var BLOCK_SIZE     = 32;
+var BLOCK_SIZE    = 32;
+var CHARA_SIZE    = 64; // 32*2
 
 var ASSETS = {
   image: {
-    mountain: "https://raw.githubusercontent.com/SilverPost/GameCenter/master/PandaRunners/image/sprite_mountain.png",
+    'mountain': 'https://raw.githubusercontent.com/SilverPost/GameCenter/master/PandaRunners/image/sprite_mountain.png',
+    'character': 'https://rawgit.com/phi-jp/phina.js/develop/assets/images/tomapiko_ss.png',
   },
   text: {
-    mountain: "https://raw.githubusercontent.com/SilverPost/GameCenter/c5afd673c66311666b8321065d1a45bc58bf3403/PandaRunners/stage/mountain.txt",
+    'mountain': 'https://raw.githubusercontent.com/SilverPost/GameCenter/c5afd673c66311666b8321065d1a45bc58bf3403/PandaRunners/stage/mountain.txt',
   },
 };
+
+/*
+ * character
+ */
+phina.define("Character", {
+  superClass: 'Sprite',
+  init: function() {
+    this.superInit('character', CHARA_SIZE, CHARA_SIZE);
+  },
+  load: function(frameIndex, start_x, start_y, group) {
+    this.addChildTo(group);
+    this.setPosition(start_x, start_y);
+    this.frameIndex = frameIndex;
+  },
+});
+
+/*
+ * player1
+ */
+phina.define("Player1", {
+  superClass: 'Character',
+  init: function() {
+    this.superInit();
+  },
+  load: function(group) {
+    this.superMethod('load', 0, BLOCK_SIZE*5, BLOCK_SIZE*10, group);
+  },
+});
 
 /*
  * stage
@@ -62,6 +92,10 @@ phina.define("MainScene", {
   
   init: function(options) {
     this.superInit(options);
+    
+    this.charaGroup = DisplayElement().addChildTo(this);
+    Player1().load(this.charaGroup);
+    
     this.blockGroup = DisplayElement().addChildTo(this);
     Stage().load('mountain', this.blockGroup);
   },
