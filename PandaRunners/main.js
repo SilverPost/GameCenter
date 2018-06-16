5db41db9dc73bb56934023dafe1e676000b80361
SilverPost <object.trade.sort+100@gmail.com>
2018-06-16 15:39:03 +0900

    [PandaRunners] Collisions player(s) and block(s).

--- PandaRunners/main.js
+++ PandaRunners/main.js
@@ -10,6 +10,11 @@ var SCREEN_HEIGHT = 640;  // 32*20
 var BLOCK_SIZE    = 32;
 var CHARA_SIZE    = 64; // 32*2
 
+// common values
+var JUMP_POWOR  = 10;
+var GRAVITY     = 0.5;
+var CAHRA_VX    = 2;
+
 var ASSETS = {
   image: {
     'mountain': 'https://raw.githubusercontent.com/SilverPost/GameCenter/master/PandaRunners/image/sprite_mountain.png',
@@ -27,6 +32,8 @@ phina.define("Character", {
   superClass: 'Sprite',
   init: function() {
     this.superInit('character', CHARA_SIZE, CHARA_SIZE);
+    this.physical.force(CAHRA_VX, 0);
+    this.isOnFloor = true;
   },
   load: function(frameIndex, start_x, start_y, group) {
     this.addChildTo(group);
@@ -44,7 +51,7 @@ phina.define("Player1", {
     this.superInit();
   },
   load: function(group) {
-    this.superMethod('load', 0, BLOCK_SIZE*5, BLOCK_SIZE*10, group);
+    this.superMethod('load', 0, BLOCK_SIZE*5, BLOCK_SIZE*13, group);
   },
 });
 
@@ -71,7 +78,7 @@ phina.define("Stage", {
   },
   layout: function(stageName, blocks, group) {
     for (var i = 0; i < blocks.length; i++) {
-      for (var j = 0; j < blocks[i].length; j++) {
+      for (var j = 0; j < blocks[0].length; j++) {
         if(blocks[i][j] !== ' ') {
           var block = Sprite(stageName, BLOCK_SIZE, BLOCK_SIZE).addChildTo(group);
           var x = j*BLOCK_SIZE+BLOCK_SIZE/2;
@@ -94,13 +101,49 @@ phina.define("MainScene", {
     this.superInit(options);
     
     this.charaGroup = DisplayElement().addChildTo(this);
-    Player1().load(this.charaGroup);
+    this.player1 = Player1();
+    this.player1.load(this.charaGroup);
     
     this.blockGroup = DisplayElement().addChildTo(this);
     Stage().load('mountain', this.blockGroup);
+    
+    this.onpointend = function() {
+      if (this.player1.isOnFloor) {
+        this.player1.physical.velocity.y = -JUMP_POWOR;
+        this.player1.physical.gravity.y = GRAVITY;
+//        player1.anim.gotoAndPlay('jump');
+      }
+    };
   },
-  
   update: function(app) {
+    this.xCollisionWith2Groups(this.charaGroup, this.blockGroup);
+    this.yCollisionWith2Groups(this.charaGroup, this.blockGroup);
+  },
+  xCollisionWith2Groups: function(attacks, defences) {
+    attacks.children.some( function(attack) {
+      var newRect = Rect(attack.left+1, attack.top, attack.width, attack.height);
+      defences.children.some( function(defence) {
+        if(Collision.testRectRect(newRect, defence)) {
+          attack.right = defence.left;
+          attack.physical.velocity.x = 0;
+        } else {
+          attack.physical.velocity.x = CAHRA_VX;
+        }
+      });
+    });
+  },
+  yCollisionWith2Groups: function(attacks, defences) {
+    attacks.children.some( function(attack) {
+      var newRect = Rect(attack.left, attack.top-1, attack.width, attack.height);
+      defences.children.some( function(defence) {
+        if(Collision.testRectRect(newRect, defence)) {
+          attack.bottom = defence.top;
+          attack.physical.velocity.y = 0;
+          attack.physical.gravity.y = 0;
+          attack.isOnFloor = true;
+        }
+      });
+    });
   },
 });
 
@@ -119,7 +162,7 @@ phina.main(function() {
   });
   
   // enable FPS
-//  app.enableStats();
+  app.enableStats();
   
   app.run();
 });
