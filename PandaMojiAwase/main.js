8300220e18391c0ac66ca29e121480eeee868cf7
SilverPost <object.trade.sort+100@gmail.com>
2018-08-18 23:15:03 +0900

    [PandaMojiAwase] Add index.html and main.js.

---
+++ PandaMojiAwase/main.js
@@ -0,0 +1,47 @@
+/*
+ * Panda moji awase
+ */
+
+phina.globalize();
+
+// size information
+var SCREEN_WIDTH  = 960;
+var SCREEN_HEIGHT = 1280;
+
+/*
+ * main function
+ */
+phina.main(function() {
+  // create application
+  var app = GameApp({
+    width: SCREEN_WIDTH,
+    height: SCREEN_HEIGHT,
+//    assets: ASSETS,
+    startLabel: 'title',
+    scenes: [
+      {
+        label: "title",
+        className: "TitleScene",
+        nextLabel: "game",
+      },
+      {
+        label: "game",
+        className: "GameScene",
+        nextLabel: "title",
+      },
+    ]
+  });
+  
+  // a dummy function that sound(s) can play with smartphone(s)
+  app.domElement.addEventListener('touchend', function dummy() {
+    var s = phina.asset.Sound();
+    s.loadFromBuffer();
+    s.play().stop();
+    app.domElement.removeEventListener('touchend', dummy);
+  });
+
+  // enable FPS
+  app.enableStats();
+  
+  app.run();
+});
