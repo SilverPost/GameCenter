0e9c4b265c6aabd4c9abe4f0bf1f8c65c1e2cd1e
SilverPost <object.trade.sort+100@gmail.com>
2018-09-30 15:50:03 +0900

    [PandaClockQuiz] Set angles to a short and long hand.

--- PandaClockQuiz/main.js
+++ PandaClockQuiz/main.js
@@ -11,8 +11,10 @@ var TITLE_LOGO_WIDTH    = 600;
 var TITLE_LOGO_HEIGHT   = 192;
 var TITLE_PANDA_WIDTH   = 611;
 var TITLE_PANDA_HEIGHT  = 575;
-var CLOCK_BG_WIDTH  = SCREEN_WIDTH*0.8;
+var CLOCK_BG_WIDTH  = SCREEN_WIDTH*0.6;
 var CLOCK_BG_HEIGHT = CLOCK_BG_WIDTH/1204*1188;
+var NEEDLE_WIDTH  = 63;
+var NEEDLE_HEIGHT = 1090;
 
 // font information
 var FONT_FAMILY = "Verdana, Roboto, 'Droid Sans', 'Hiragino Kaku Gothic ProN', sans-serif";
@@ -28,6 +30,20 @@ var ASSETS = {
     'logo': 'https://raw.githubusercontent.com/SilverPost/GameCenter/master/PandaClockQuiz/image/title_logo.png',
     'panda': 'https://raw.githubusercontent.com/SilverPost/GameCenter/master/PandaClockQuiz/image/title_panda.png',
     'clock': 'https://raw.githubusercontent.com/SilverPost/GameCenter/master/PandaClockQuiz/image/clock.png',
+    'needle': 'https://raw.githubusercontent.com/SilverPost/GameCenter/master/PandaClockQuiz/image/needle.png',
+  },
+  spritesheet: {
+    'needle_ss':
+    {
+      "frame": {
+        "width": NEEDLE_WIDTH,
+        "height": NEEDLE_HEIGHT,
+        "cols": 1,
+        "rows": 2,
+      },
+      "animations" : {
+      }
+    },
   },
 };
 
@@ -89,25 +105,71 @@ phina.define("GameScene", {
     this.loading();
   },
   loading: function() {
-    this.clock = ClockImage().addChildTo(this);
-    this.clock.loading();
+    this.clock = Clock().addChildTo(this);
+    this.clock.loading(this);
+    // set hands (for debug)
+    this.clock.set_needles(3, 0);
   },
 });
 
 /*
  * clock image
  */
-phina.define("ClockImage", {
+phina.define("Clock", {
   superClass: "Sprite",
   init: function() {
     this.superInit('clock');
   },
-  loading: function() {
+  loading: function(self) {
+    // background image
     this.setPosition(SCREEN_WIDTH*0.5, CLOCK_BG_HEIGHT*0.6);
     this.width = CLOCK_BG_WIDTH;
     this.height = CLOCK_BG_HEIGHT;
+    // long hand
+    this.long_hand = NeedleImage().addChildTo(self);
+    this.long_hand.loading(0, this.x, this.y);
+    // short hand
+    this.short_hand = NeedleImage().addChildTo(self);
+    this.short_hand.loading(1, this.x, this.y);
   },
-})
+  set_needles: function(hour, minute) {
+    this.set_long_hand(minute);
+    this.set_short_hand(hour, minute);
+  },
+  set_long_hand: function(minute) {
+    this.long_hand.rotation = this.long_hand_angle(minute);
+  },
+  long_hand_angle: function(minute) {
+    return 360 / 60 * minute;
+  },
+  set_short_hand: function(hour, minute) {
+    this.short_hand.rotation = this.short_hand_angle(hour, minute);
+  },
+  short_hand_angle: function(hour, minute) {
+    var long_hand_angle = this.long_hand_angle(minute);
+    var short_hand_angle = (hour === 12) ? 0 : 360 / 12 * hour;
+    short_hand_angle += 360 / 12 / 60 * minute;
+    return short_hand_angle;
+  },
+});
+
+/*
+ * needle image
+ */
+phina.define("NeedleImage", {
+  superClass: "Sprite",
+  init: function() {
+    this.superInit('needle');
+  },
+  loading: function(index, x, y) {
+    this.width = NEEDLE_WIDTH;
+    this.height = NEEDLE_HEIGHT;
+    this.frameIndex = index;
+    this.setPosition(x, y);
+    this.scaleX = 0.34;
+    this.scaleY = 0.34;
+  }
+});
 
 /*
  * result scene
